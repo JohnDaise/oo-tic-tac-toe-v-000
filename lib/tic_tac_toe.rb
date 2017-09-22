67119744e26f645672190338b7c2758821dc2851
John Daise <jdaise@gmail.com>
2017-09-22 04:27:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,4 +47,16 @@ class TicTacToe
     turn_count % 2 == 0 ? "X" : "O"
   end
 
+  def turn
+    puts "Please enter 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+    if valid_move?(index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+  end
+
+
 end
